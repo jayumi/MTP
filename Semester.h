e78358c2041e74ea992e443673ff241e61cb8ca2
jintax <73630624+jintax@users.noreply.github.com>
2020-10-29 16:25:50 -0700

    Update Semester.h

--- Semester.h
+++ Semester.h
@@ -1,10 +1,12 @@
 #ifndef SEMESTER_H	
 #define SEMESTER_H
 #include <iostream>
+#include "Time.h"
 #include "Date.h"
+#include "Course.h"
 using namespace std;
 
-class Semester : public Date
+class Semester
 {
 	friend ostream& operator<<(ostream&, const Semester&);
 	friend istream& operator>>(istream&, Semester&);
@@ -15,25 +17,16 @@ private:
 	Date endDate;
 
 public:
-	Semester();
-	Semester(string, Date, Date);
+	Semester(const string & = "NULL", const Date & = Date(), const Date & = Date());
 
 	//GET
-	string geSemesterName() const;
-	int getSemStartMonth() const;
-	int getSemStartDay() const;
-	int getSemStartYear() const;
-	int getSemEndMonth() const;
-	int getSemEndDay() const;
-	int getSemEndYear() const;
+	string getSemesterName() const;
+	Date getStartDate() const;
+	Date getEndDate() const;
 
 	//SET
-	void setSemesterName(string&);
-	void setSemStartMonth(int);
-	void setSemStartDay(int);
-	void setSemStartYear(int);
-	void setSemEndMonth(int);
-	void setSemEndDay(int);
-	void setSemEndYear(int);
+	Semester& setSemesterName(string);
+	Semester& setStartDate(int, int, int);
+	Semester& setEndDate(int, int, int);
 };
 #endif // !SEMESTER_H
