15a00c663ea7e6d49f936b61ae38326586121216
jayumi <73630624+jayumi@users.noreply.github.com>
2020-10-28 22:33:23 -0700

    Create Time.h

---
+++ Time.h
@@ -0,0 +1,32 @@
+#include <string>
+using namespace std;
+#ifndef TIME_H
+#define TIME_H
+
+class Time
+{
+	friend ostream& operator<<(ostream&, const Time&);
+	friend istream& operator>>(istream&, Time&);
+
+private:
+	int hour;
+	int minute;
+	void convertUniversal(int, int, string);
+
+public:
+	Time(int = 0, int = 0);
+
+	Time& setTime(int, int);
+	Time& setHour(int);
+	Time& setMinute(int);
+
+	int getHour() const;
+	int getMinute() const;
+
+	double operator-(const Time&); //overload - operator
+
+	void printUniversal() const;
+	void printStandard() const;
+
+};
+#endif // !TIME_H
