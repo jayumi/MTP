7370560d4d0e57fdfa8e73a105d75666b57f7dfb
jintax <73630624+jintax@users.noreply.github.com>
2020-10-29 18:02:33 -0700

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -8,11 +8,6 @@ Date::Date(int m, int d, int y)
 	setDate(m, d, y);
 }
 
-Date::~Date()
-{
-
-}
-
 Date& Date::setDate(int m, int d, int y)
 {
 	setMonth(m);
@@ -22,45 +17,40 @@ Date& Date::setDate(int m, int d, int y)
 	return *this;
 }
 
-void Date::checkDate()
+int Date::checkDate(int check) const
 {
 	static const int daysPerMonth[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	if (month <= 0 || month >= 12)
+	if (check > 0 && check <= daysPerMonth[month])
 	{
-		month = 1;
+		return check;
 	}
 
-	if (!(month == 2 && day == 29 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
+	if ((month == 2 && day == 29 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
 		&& (day <= 0 || day > daysPerMonth[month]))
 	{
-		day = 1;
+		return check;
 	}
 
-	if (year <= 1989)
-	{
-		year = 1990;
-	}
+	cout << "Invalid Day " << check << " was set to 1" << endl;
+	return 1;
 }
 
 Date& Date::setMonth(int m)
 {
-	month = m;
-	checkDate();
+	month = (m <= 1 || m <= 12) ? m : 0;
 	return *this;
 }
 
 Date& Date::setDay(int d)
 {
 	day = d;
-	checkDate();
 	return *this;
 }
 
 Date& Date::setYear(int y)
 {
-	year = y;
-	checkDate();
+	year = (y > 0) ? y : 0;
 	return *this;
 }
 
@@ -81,88 +71,108 @@ int Date::getYear() const
 
 bool Date::operator<(const Date& right)
 {
-	bool status;
 	if (year < right.year)
-		status = true;
-	else if ((year == right.year) && (month < right.month))
-		status = true;
+	{
+		return true;
+	}
+	else if ((year == right.year) && (month < right.month)) 
+	{
+		return true;
+	}
 	else if ((year == right.year) && (month < right.month) && (day < right.day))
-		status = true;
+	{
+		return false;
+	}
 	else
-		status = false;
-
-	return status;
+	{
+		return false;
+	}
+	return false;
 }
 
 bool Date::operator>(const Date& right)
 {
-	bool status;
 	if (year > right.year)
-		status = true;
+	{
+		return true;
+	}
 	else if ((year == right.year) && (month > right.month))
-		status = true;
+	{
+		return true;
+	}
 	else if ((year == right.year) && (month > right.month) && (day > right.day))
-		status = true;
+	{
+		return false;
+	}
 	else
-		status = false;
-
-	return status;
+	{
+		return false;
+	}
+	return false;
 }
 
 bool Date::operator<=(const Date& right)
 {
-	bool status;
 	if (year <= right.year)
-		status = true;
+	{
+		return true;
+	}
 	else if ((year == right.year) && (month <= right.month))
-		status = true;
+	{
+		return true;
+	}
 	else if ((year == right.year) && (month <= right.month) && (day <= right.day))
-		status = true;
+	{
+		return false;
+	}
 	else
-		status = false;
-
-	return status;
+	{
+		return false;
+	}
+	return false;
 }
 
 bool Date::operator>=(const Date& right)
 {
-	bool status;
-	if (year < right.year)
-		status = true;
-	else if ((year == right.year) && (month < right.month))
-		status = true;
-	else if ((year == right.year) && (month < right.month) && (day < right.day))
-		status = true;
+	if (year >= right.year)
+	{
+		return true;
+	}
+	else if ((year == right.year) && (month >= right.month))
+	{
+		return true;
+	}
+	else if ((year == right.year) && (month >= right.month) && (day >= right.day))
+	{
+		return false;
+	}
 	else
-		status = false;
-
-	return status;
+	{
+		return false;
+	}
+	return false;
 }
 
 bool Date::operator==(const Date& right)
 {
-	bool status;
-	if (year == right.year && month == right.month && day == right.day)
-		status = true;
-	else
-		status = false;
-	return status;
+	return (month == right.month && day == right.day && year == right.year);
 }
 
 bool Date::operator!=(const Date& right)
 {
-	bool status;
 	if (year != right.year && month != right.month && day != right.day)
-		status = true;
-	else
-		status = false;
-	return status;
+		return true;
+	else 
+	{
+		return false;
+	}
+	return false;
 }
 
 ostream& operator<<(ostream& output, const Date& obj)
 {
-	output << setfill('0') << setw(2) << obj.month << '/' << setfill('0') << setw(2) << obj.day << '/' << obj.year;
-
+	output << setfill('0') << setw(2) << obj.month << '/' << setfill('0') 
+		<< setw(2) << obj.day << '/' << obj.year;
 	return output;
 }
 
@@ -173,7 +183,5 @@ istream& operator>>(istream& input, Date& obj)
 	input >> obj.day;
 	input.ignore();
 	input >> obj.year;
-
-	obj.checkDate();
 	return input;
 }
