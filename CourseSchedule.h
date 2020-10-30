6cb8e80ea2bd1816f0e377639288d354faf33609
jintax <73630624+jintax@users.noreply.github.com>
2020-10-29 23:42:20 -0700

    Create CourseSchedule.h

---
+++ CourseSchedule.h
@@ -0,0 +1,38 @@
+#ifndef COURSESCHEDULE_H
+#define COURSESCHEDULE_H
+#include <iostream>
+#include "Date.h"
+#include "Time.h"
+#include "Semester.h"
+#include "Course.h"
+using namespace std;
+
+class CourseSchedule
+{
+	friend ostream& operator<<(ostream&, const CourseSchedule&);
+
+private:
+	string studentName;
+	Semester semester;
+	Course* coursePtr;
+	int maxSize; //max number of courses
+	int numOfCourses;
+	bool checkDates(const Semester&, const Date&, const Date&) const; //utility function
+
+public:
+	CourseSchedule(const string& = "Student Name",const Semester& = Semester(), int = 0);
+	CourseSchedule(const CourseSchedule&);
+	~CourseSchedule();
+
+	//GET
+	string getStudentName() const;
+	Semester getSemester() const;
+	int getNumOfCourses() const;
+
+	//SET
+	void setStudentName(string);
+	
+	bool addCourse(const Course&);
+	bool removeCourse(const Course&);
+};
+#endif // !CourseSchedule_H
