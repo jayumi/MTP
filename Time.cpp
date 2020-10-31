c8300f83b2b08c5d871c70419dbe93a4f1d29bd9
jintax <73630624+jintax@users.noreply.github.com>
2020-10-30 19:24:33 -0700

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -1,119 +1,127 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cmath>
 #include <algorithm>
-#include "Time.h"
 using namespace std;
+
+#include "Time.h"
 
 //Constructor
 Time::Time(int hour, int minute)
 {
-	setTime(hour, minute);
+    setTime(hour, minute);
 }
 
 //convert time to universal
-void Time::convertUniversal(int hr, int min, bool mer)
+void Time::convertUniversal(int hr, int min, string mer)
 {
-	if (hr == 0)
-	{
-		hr = 12;
-	}
-	else if (hr == 12)
-	{
-		hr = 0;
-	}
-
-	if (hr <= 12 && hr >= 0)
-	{
-		if (mer)
-		{
-			setTime(hr, min);
-		}
-		else
-		{
-			setTime(hr + 12, min);
-		}
-	}
+    if (hr == 12)
+    {
+        hr = 0;
+    }
+    if (mer == "AM")
+    {
+        setTime(hr, min);
+    }
+    else
+    {
+        setTime(hr + 12, min);
+    }
+    //for troubleshooting
+    //cout << hour << ":" << minute << endl;
 }
 
 //SET FUNCTIONS
+
 Time& Time::setTime(int hour, int minute)
 {
-	setHour(hour);
-	setMinute(minute);
-	return *this;
+    setHour(hour);
+    setMinute(minute);
+    return *this;
 }
+
 
 Time& Time::setHour(int h)
 {
-	hour = (h >= 0 && h < 24) ? h : 0;
-	return *this;
+    hour = (h >= 0 && h < 24) ? h : 0;  //validates hour, if valid set to h, else set to 0
+    return *this;
 }
+
 
 Time& Time::setMinute(int m)
 {
-	minute = (m >= 0 && m < 60) ? m : 0;
-	return *this;
+    minute = (m >= 0 && m < 60) ? m : 0;  //validates minute, if valid set to m, else set to 0
+    return *this;
 }
 
 //GET FUNCTIONS
-int Time::getHour() const
+
+//get Hour
+int Time::getHour() const  //must be const since prototype is const
 {
-	return hour;
+    return hour;
 }
 
-int Time::getMinute() const
+//get Minute
+int Time::getMinute() const  //must be const since prototype is const
 {
-	return minute;
+    return minute;
 }
 
-//PRINT FUNCTIONS
-void Time::printUniversal() const
+
+/*PRINT FUNCTIONS:  Normally do not modify private member data so should be const*/
+
+void Time::printUniversal()const  //must be const since prototype is const
 {
-	cout << setfill('0') << setw(2) << hour << ":"
-		<< setw(2) << minute << endl;
+    cout << setfill('0') << setw(2) << hour << ":" << setw(2) << minute << endl;
 }
 
-void Time::printStandard() const
+void Time::printStandard()const  //must be const since prototype is const
 {
-	cout << ((hour == 0 || hour == 12) ? 12 : hour % 12) << ":"
-		<< setfill('0') << setw(2) << minute << " "
-		<< (hour < 12 ? "AM" : "PM") << endl;
+    cout << ((hour == 0 || hour == 12) ? 12 : hour % 12) << ":"
+        << setfill('0') << setw(2) << minute << " "
+        << (hour < 12 ? "AM" : "PM") << endl;
 }
 
-//Overload -operator
+// - overloaded operator
 double Time::operator-(const Time& t1)
 {
-	double timeDiff;
-	timeDiff = double(hour) + double(minute) / 60.0 - (double(t1.hour) - double(t1.minute) / 60.0);
-	return abs(timeDiff);
+    double timeDif;
+    timeDif = abs((double(getHour()) + double(getMinute()) / 60) - (double(t1.hour) + double(t1.minute) / 60));
+    return timeDif;
 }
 
-// << overload operator
-ostream& operator <<(ostream& output, const Time& t1)
+
+
+
+// << overloaded operator
+ostream& operator<<(ostream& output, const Time& t1)
 {
-	output << ((t1.hour == 0 || t1.hour == 12) ? 12 : t1.hour % 12)
-		<< ":" << setfill('0') << t1.minute << (t1.hour < 12 ? "AM" : "PM");
-	return output;
+    int hr = t1.hour;
+    int min = t1.minute;
+    output << ((hr == 0 || hr == 12) ? 12 : hr % 12) << ":"
+        << setfill('0') << setw(2) << min << " " << (hr < 12 ? "AM" : "PM") << endl;
+
+    return output;
 }
 
-istream& operator >>(istream& input, Time& t1)
+// >> overloaded operator
+istream& operator>>(istream& input, Time& t1)
 {
-	int hr, min;
-	string meridiem;
-	input >> setw(2) >> hr;
-	input.ignore();
-	input >> setw(2) >> min;
-	input.ignore();
-	input >> setw(2) >> meridiem;
+    int hr, min;
+    string meridiem;
+    input >> setw(2) >> hr;
+    input.ignore();
+    input >> setw(2) >> min;
+    input.ignore();
+    input >> setw(2) >> meridiem;
+    // cout << "For trouble shooting" << hr << " " << min << endl;
+    transform(meridiem.begin(), meridiem.end(), meridiem.begin(), ::toupper);
+    t1.convertUniversal(hr, min, meridiem);
+    return input;
 
-	if (meridiem == "AM" || meridiem == "am")
-	{
-		t1.convertUniversal(hr, min, false);
-	}
-	else if (meridiem == "PM" || meridiem == "pm")
-	{
-		t1.convertUniversal(hr, min, false);
-	}
-	return input;
 }
+
+
+
